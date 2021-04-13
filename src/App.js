6f78c60a90f4722f63e3a3d7d24a83a71c0781cf
takumi0910 <takedagen0910@gmail.com>
2021-04-13 16:09:31 +0900

    予定登録機能エラー修正

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default class App extends React.Component {
     this.state = {
       isSubmitted: true,
       //月のデータ
-      month_days: '',
+      month_days: {},
       selectedDate: '',
       backups: '',
       formvalues: '',
@@ -35,7 +35,7 @@ export default class App extends React.Component {
 
   //日付ブロックをクリックした時の処理
   openModal(value, e) {
-    if (e.target.title === 'delite') {
+    if (e.target.title === 'delete') {
       this.setState({ isSubmitted: true })
     } else {
       this.setState({ isSubmitted: false })
@@ -63,7 +63,7 @@ export default class App extends React.Component {
           this.state.month_days[day] && this.state.month_days[day].map(date => {
             return (
               <div className='plans'>
-                <button className={day} id={date.id} title='delite' onClick={this.deleteState}>×</button>
+                <button className={day} id={date.id} title='delete' onClick={this.deleteState}>×</button>
                 <button className={day} id={date.id} onClick={this.editState}>{date.text}</button>
                 <br />
               </div>
@@ -128,15 +128,16 @@ export default class App extends React.Component {
     let end_h = this.state.end_hour
     let end_dh = this.state.backups[2]
 
-    if (limited_h === '' && this.state.backups[0] === undefined) {
-    } else if (limited_h === '' && this.state.backups[0] !== '') {
+    if (!limited_h && !this.state.backups[0]) {
+    } else if (!limited_h && this.state.backups[0]) {
       limited_h = this.state.backups[0]
     }
 
-    if(end_h === '' && end_dh !== undefined){
+    if (!end_h && end_dh) {
       end_h = end_dh
     }
 
+    //疑惑の判定
     for (var i = 0; i <= 23; i++) {
       if (i >= limited_h) {
         if (i != end_h) {
@@ -174,33 +175,33 @@ export default class App extends React.Component {
     let limited_minutes = this.state.start_minute
 
     //分数制限の値の算出
-    if (limited_minutes === undefined && start_dm !== '') {
+    if (!limited_minutes && start_dm) {
       limited_minutes = start_dm
-    } else if (limited_minutes === undefined && start_dm === '') {
+    } else if (!limited_minutes && start_dm) {
       limited_minutes = 0
     }
 
     //初めの時間を取得
-    if (start_h === '' && start_dh === undefined) {
+    if (!start_h && !start_dh) {
       limited_start = ''
-    } else if (start_h === '' && start_dh !== undefined) {
+    } else if (!start_h && start_dh) {
       limited_start = start_dh
     } else {
       limited_start = start_h
     }
 
     //終わりの時間を取得
-    if (end_h === '' && end_dh === undefined && limited_start === '') {
+    if (!end_h && !end_dh && !limited_start) {
       limited_end = ''
-    } else if (end_h === '' && end_dh !== '' && limited_start === '') {
+    } else if (!end_h && end_dh && !limited_start) {
       limited_end = end_dh
-    } else if (end_h === '' && limited_start === start_dh) {
+    } else if (!end_h && limited_start === start_dh) {
       limited_end = start_dh
-    } else if (end_h === '' && start_h !== '') {
+    } else if (!end_h && start_h) {
       limited_end = start_h
-    } else if (end_h !== '' && start_h > end_h) {
+    } else if (end_h && start_h > end_h) {
       limited_end = limited_start
-    } else if (end_h !== '') {
+    } else if (end_h) {
       limited_end = end_h
     }
 
@@ -242,10 +243,10 @@ export default class App extends React.Component {
 
   //予定追加or予定編集処理
   handleSubmit() {
-    //モーダルの非表示
     this.setState({ isSubmitted: true })
 
-    const date = this.getFormatDate(new Date(this.state.selectedDate))
+    let date = this.getFormatDate(new Date(this.state.selectedDate))
+    date = Number(date)
 
     const copySate = this.state.month_days
 
@@ -255,6 +256,7 @@ export default class App extends React.Component {
 
     const random_id = this.getUniqueStr()
 
+
     //stateを変数に代入
     let start_h = this.state.start_hour
     let start_m = this.state.start_minute
@@ -263,41 +265,41 @@ export default class App extends React.Component {
     let form = this.state.formvalues
 
     // 予定が始まる時間が空か確かめる（1時間単位）
-    if (start_h === '' && this.state.backups[0] !== undefined) {
+    if (!start_h && this.state.backups[0]) {
       start_h = this.state.backups[0]
-    } else if (start_h === '' && this.state.backups[0] == undefined) {
+    } else if (!start_h && !this.state.backups[0]) {
       start_h = '0'
     }
 
     //予定が始まる時間が空か確かめる（1分単位）
-    if (start_m === '' && this.state.backups[1] !== undefined) {
+    if (!start_m && this.state.backups[1]) {
       start_m = this.state.backups[1]
-    } else if (start_m === '' && this.state.backups[1] == undefined) {
+    } else if (!start_m && !this.state.backups[1]) {
       start_m = '00'
     }
 
     //予定が終わる時間が空か確かめる（1時間単位）
-    if (end_h === '' && this.state.backups[2] !== undefined) {
+    if (!end_h && this.state.backups[2]) {
       if (this.state.start_hour > this.state.backups[2]) {
         end_h = this.state.start_hour
       } else {
         end_h = this.state.backups[2]
       }
-    } else if (end_h === '' && this.state.backups[2] == undefined && start_h !== '') {
+    } else if (!end_h && !this.state.backups[2] && start_h) {
       end_h = start_h
     }
 
     //予定が終わる時間が空か確かめる（1分単位）
-    if (end_m === '' && this.state.backups[3] !== undefined) {
+    if (!end_m && this.state.backups[3]) {
       end_m = this.state.backups[3]
-    } else if (end_m === '' && this.state.backups[3] == undefined && start_m !== '') {
+    } else if (!end_m && !this.state.backups[3] && start_m) {
       end_m = start_m
     }
 
     //予定内容が空か確かめる
-    if (form === '' && this.state.backups[4] !== '') {
+    if (!form && this.state.backups[4]) {
       form = this.state.backups[4]
-    } else if (form === '' && this.state.backups[4] == '') {
+    } else if (!form && !this.state.backups[4]) {
       form = ''
     }
 
@@ -305,18 +307,16 @@ export default class App extends React.Component {
     if (end_h < start_h) {
       end_h = start_h
     }
-
     if (start_h === end_h && start_m > end_m) {
       end_m = start_m
     }
 
-    //ここから下はhandleSubmit短縮不能
     //予定の開始時間と終了時間を出力する表示に変更
     let start_time = start_h + ':' + start_m
     let end_time = end_h + ':' + end_m
 
 
-    if (form === undefined && this.state.backups[4] === undefined) {
+    if (!form && !this.state.backups[4]) {
       alert('予定を入力して下さい')
     }
     else if (index !== -1) {
@@ -326,7 +326,9 @@ export default class App extends React.Component {
         backup: [start_h, start_m, end_h, end_m, form]
       })
     }
-    else if (form !== "" && start_time !== ':' || end_time !== ':') {
+    else if (form && start_time !== ':' || end_time !== ':') {
+      console.log(date)
+      console.log(typeof date);
       copySate[date] = [
         {
           id: random_id,
@@ -335,7 +337,6 @@ export default class App extends React.Component {
         }
       ]
     }
-
     this.setState({ month_days: copySate })
     this.setState({ backups: '' })
     this.setState({ start_hour: '' })
@@ -344,8 +345,6 @@ export default class App extends React.Component {
     this.setState({ end_minute: '' })
     this.setState({ formvalues: '' })
   }
-
-
 
   //予定を編集する処理
   editState = (e) => {
@@ -361,8 +360,6 @@ export default class App extends React.Component {
       }
     }
   }
-
-
 
   //予定を消す処理
   deleteState = (e) => {
@@ -384,8 +381,6 @@ export default class App extends React.Component {
     }
   }
 
-
-
   // もし前回のデータがあったら、ローカルストレージの値を取得し、更新する
   componentDidMount() {
     if (localStorage.app) {
@@ -396,18 +391,16 @@ export default class App extends React.Component {
     }
   }
 
-
-
   // stateが変更されたら実行
   componentDidUpdate() {
     // ローカルストレージにステートの情報を保存
     localStorage.setItem('app', JSON.stringify(this.state));
+    // localStorage.clear()
   }
-
-
 
   render() {
     const title = ({ date, view }) => this.getTileContent({ date, view })
+    console.log(this.state)
     return (
       <>
         <Calendar
